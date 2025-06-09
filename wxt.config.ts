775f8fdbade588211d0312a5076c1896ee053a04
Seya <31957516+sglkc@users.noreply.github.com>
2025-06-09 17:01:23 +0700

    init commit

---
+++ wxt.config.ts
@@ -0,0 +1,25 @@
+import { preact } from '@preact/preset-vite'
+import { defineConfig } from 'wxt'
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  manifest: {
+    permissions: ['activeTab', 'declarativeContent', 'storage'],
+    host_permissions: ['https://translate.google.com/*'],
+    externally_connectable: {
+      ids: ['*'],
+      matches: ['https://open.spotify.com/*'],
+      accepts_tls_channel_id: false,
+    },
+  },
+  modules: ['@wxt-dev/unocss'],
+  imports: false,
+  webExt: {
+    chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
+  },
+  vite: () => ({
+    plugins: [
+      preact(),
+    ],
+  }),
+});
